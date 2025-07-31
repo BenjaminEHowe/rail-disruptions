3ca6000e5c973aff38288a1a1de62bc9beafb072
Benjamin Howe <ben@beh.uk>
2025-07-31 22:43:39 +0100

    Update cached data every 5 minutes

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+import apscheduler.schedulers.background
 import flask
+import logging
 import os
 
 import api
 
 
 app = flask.Flask(__name__)
-
-incidentDetails = {}
+logger = logging.getLogger(__name__)
 nrDisruptions = api.NRDisruptionsClient(
   base_url=os.environ.get("NR_API_BASE_URL"),
   api_key=os.environ.get("NR_API_KEY"),
 )
-serviceIndicators = nrDisruptions.get_toc_service_indicators()
-serviceIndicators.sort(key=lambda x: x.operator.name.lower())
-for serviceIndicator in serviceIndicators:
-  for incident in serviceIndicator.incidents:
-    if incident.id not in incidentDetails:
-      incidentDetails[incident.id] = nrDisruptions.get_incident_details(incident.id)
+scheduler = apscheduler.schedulers.background.BackgroundScheduler()
+
+# TODO: migrate the below state into a database
+incidentDetails = {}
+serviceIndicators = []
 
 
 @app.route("/")
@@ -26,3 +26,22 @@ def all_operators():
     incidentDetails=incidentDetails,
     serviceIndicators=serviceIndicators,
   )
+
+
+def refresh_cached_data():
+  global incidentDetails, serviceIndicators
+  logger.info("Getting updated data from API")
+  incidentDetails = {}
+  serviceIndicators = nrDisruptions.get_toc_service_indicators()
+  serviceIndicators.sort(key=lambda x: x.operator.name.lower())
+  for serviceIndicator in serviceIndicators:
+    for incident in serviceIndicator.incidents:
+      if incident.id not in incidentDetails:
+        incidentDetails[incident.id] = nrDisruptions.get_incident_details(incident.id)
+  logger.info("Cached data updated successfully")
+
+
+logging.basicConfig(level=logging.INFO)
+refresh_cached_data()
+scheduler.start()
+scheduler.add_job(refresh_cached_data, "interval", minutes=5)

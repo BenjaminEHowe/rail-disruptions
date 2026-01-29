e867e59348b084ef1b0f44990b9829dc43f76a6e
Benjamin Howe <ben@beh.uk>
2026-01-29 23:09:44 +0000

    Create parser

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 import os
 
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from typing import Optional
 
 from model import Base
+from model import Incident
+from model import IncidentVersion
 from model import RawData
 
 
@@ -13,12 +16,31 @@ engine = create_engine(connection_string)
 Base.metadata.create_all(engine)
 
 
+def get_session():
+  return Session(engine)
+
+
 def insert_raw_data(raw_data: RawData):
   with Session(engine) as session:
     session.add(raw_data)
     session.commit()
 
 
+def select_incident_where_nr_id(nr_id: str, session: Session = get_session()) -> Optional[Incident]:
+  with session:
+    return session.query(Incident).where(Incident.nr_id == nr_id).first()
+
+
+def select_incident_version_where_incident_id_and_updated(incident_id: str, updated_ts: datetime, session: Session = get_session()) -> Optional[IncidentVersion]:
+  with session:
+    return session.query(IncidentVersion).where(IncidentVersion.incident_id == incident_id, IncidentVersion.updated_ts == updated_ts).first()
+
+
 def select_latest_raw_data() -> Optional[RawData]:
   with Session(engine) as session:
-    return session.query(RawData).order_by(RawData.id.desc()).first()
+    return session.query(RawData).order_by(RawData.retrieved.desc()).first()
+
+
+def select_oldest_unparsed_raw_data() -> Optional[RawData]:
+  with Session(engine) as session:
+    return session.query(RawData).where(RawData.parsed == False).order_by(RawData.retrieved.asc()).first()

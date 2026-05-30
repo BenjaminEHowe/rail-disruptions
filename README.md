d0b2fd6fabf909bb469933bf407780e1c5b876b2
Benjamin Howe <ben@beh.uk>
2026-05-30 09:47:28 +0100

    Switch ingester to use object storage

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 Experiments with the [National Rail Disruptions API](https://raildata.org.uk/dashboard/dataProduct/P-4b85a789-5f14-4763-841a-b3ada659a51a/overview).
 
 ## Environment variables
+The following environment variables are required:
+- `S3_ENDPOINT`: the S3-compatible object storage endpoint.
+- `S3_ACCESS_KEY`: the access key to use for S3.
+- `S3_SECRET_KEY`: the secret key to use for S3.
+- `S3_BUCKET`: the bucket to store the objects in (must already exist).
+
 The following environment variables may be set in customise how the application behaves:
 - `NR_KB_PROXY_URL`: the base URL for an instance of [the National Rail KnowledgeBase Proxy](https://github.com/BenjaminEHowe/national-rail-knowledgebase-proxy), defaults to `https://nrkbproxy.beh.uk`.
+- `S3_SECURE`: whether to use a secure connection when connecting to S3, defaults to `1`.
 
 ## Development
 To start the application in development, run a command like:
--- app/ingester.py
+++ app/ingester.py
@@ -1,15 +1,32 @@
 import logging
+import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 from flask import Flask
 from hashlib import sha256
+from io import BytesIO
+from minio import Minio
 from requests import get
 
-from database import insert_raw_data
-from database import select_latest_raw_data
-from model import RawData
+
+def get_env(name, default=None):
+  var = os.environ.get(name)
+  if var is not None:
+    return var
+  if default is not None:
+    return default
+  raise RuntimeError(f"Environment variable {name} is required!")
 
 
+bucket = get_env("S3_BUCKET")
+client = Minio(
+    get_env("S3_ENDPOINT"),
+    access_key=get_env("S3_ACCESS_KEY"),
+    secret_key=get_env("S3_SECRET_KEY"),
+    region="unused",
+    secure=bool(get_env("S3_SECURE", True)),
+)
 flask_app = Flask(__name__)
 logger = logging.getLogger(__name__)
 scheduler = BackgroundScheduler()
@@ -17,35 +34,48 @@ scheduler = BackgroundScheduler()
 
 @flask_app.route("/")
 def status():
-  latest_raw_data = select_latest_raw_data()
-  if latest_raw_data is not None:
-    latest_raw_data_hash = latest_raw_data.hash
-    latest_raw_data_retrieved = latest_raw_data.retrieved
-  else:
-    latest_raw_data_hash = None
-    latest_raw_data_retrieved = None
   return {
-    "latest_data_hash": latest_raw_data_hash,
-    "latest_data_retrieved": latest_raw_data_retrieved,
+    "latest_data_hash": latest_hash,
+    "latest_data_retrieved": latest_data_received,
   }
 
 
+def get_latest_hash_from_storage():
+  logger.info("Retrieving stored data from S3")
+  objects = client.list_objects(bucket, recursive=True)
+  filenames = sorted([obj.object_name for obj in objects], reverse=True)
+  if filenames:
+    return filenames[0].split(".")[0].split("_")[1]
+  return None
+
+
 def get_latest_data():
+  global latest_data_received, latest_hash
   logger.info("Getting latest data")
   current_incidents = get("https://nrkbproxy.beh.uk/incidents.xml", headers={"user-agent": "not-requests"}).text
   current_hash = sha256(current_incidents.encode()).digest().hex()
-  latest_raw_data = select_latest_raw_data()
-  if latest_raw_data is not None and latest_raw_data.hash == current_hash:
+  if latest_hash == current_hash:
     logger.info("Current data is up-to-date")
   else:
     logger.info("Updated data retrieved, storing...")
-    insert_raw_data(RawData(
-      hash=current_hash,
-      data=current_incidents,
-    ))
+    now = datetime.now().strftime("%Y-%m-%dT%H%M%S")
+    filename = f"{now}_{current_hash}.xml"
+    data = current_incidents.encode("utf-8")
+    client.put_object(
+      bucket,
+      filename,
+      BytesIO(data),
+      length=len(data),
+      content_type="application/xml",
+    )
+    latest_data_received = now
+    latest_hash = current_hash
+
 
 
 logging.basicConfig(level=logging.INFO)
+latest_data_received = None
+latest_hash = get_latest_hash_from_storage()
 get_latest_data()
 scheduler.start()
 scheduler.add_job(get_latest_data, "interval", minutes=1)
